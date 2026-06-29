3d7cd9892e3e69bbbfd5aec8f6da3e7eb7ffeb1f
Neil Cameron <neil@neilacameron.com>
2026-06-29 17:16:58 +1000

    Update public build 20260629.2

--- dist/Ditherloom-Suite-v0.2.8-Updater.ps1
+++ dist/Ditherloom-Suite-v0.2.8-Updater.ps1
@@ -4,8 +4,8 @@ param(
 
 $ErrorActionPreference = 'Stop'
 $packageUrl = 'https://github.com/nacameron/Ditherloom-Suite/releases/download/v0.2.8/Ditherloom-Suite-v0.2.8.zip'
-$expectedHash = '872B2FA7AEB63F1E4C00C8C790E97531A902B742A9041FB7C9D0FFCEF39B2A19'
-$expectedExeHash = '629F26CC80929484C2605F5B3834F85F5F94446B51E04121EF70712C68CB8EDD'
+$expectedHash = '940F89F041AC90986AD820E84FDF7761D814CDA6AA37DE1512BFD6D15794BD12'
+$expectedExeHash = 'D231BDF3B26B60D8CF805A7011CFFF2445E9B9B2D21E32E0F1CBB1B2AED73BEA'
 $packageName = 'Ditherloom-Suite-v0.2.8.zip'
 
 Add-Type -AssemblyName System.Windows.Forms
@@ -142,7 +142,7 @@ function Write-UpdateContinuityMarker([string]$target) {
         appName = 'Ditherloom Suite'
         purpose = 'in-place-update'
         targetVersion = '0.2.8'
-        targetBuildId = '20260629.1'
+        targetBuildId = '20260629.2'
         writtenAt = (Get-Date).ToUniversalTime().ToString('o')
     }
     $payload | ConvertTo-Json -Depth 4 | Set-Content -LiteralPath $markerPath -Encoding UTF8
